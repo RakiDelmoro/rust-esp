fe2fedbd8431fe37a4b5acdddf235a6adee1e408
Raki Delmoro <rakidelmoro@gmail.com>
2026-02-10 04:12:02 +0000

    Fix bug

--- water-flow/src/main.rs
+++ water-flow/src/main.rs
@@ -34,33 +34,20 @@ fn time_now_in_millis() -> u64 {
 
 fn setup_wifi(modem: Modem) -> anyhow::Result<EspWifi<'static>> {
     let ssid_as_heap_string: String<32> = String::try_from(WIFI_SSID).expect("SSID too long");
-    let password_as_heap_string: String<64> =
-        String::try_from(WIFI_PASSWORD).expect("Password too long");
+    let password_as_heap_string: String<64> = String::try_from(WIFI_PASSWORD).expect("Password too long");
 
     let sysloop = EspSystemEventLoop::take().expect("Failed to take event loop");
     let nvs = EspDefaultNvsPartition::take().expect("Failed to take NVS");
 
-    let mut wifi =
-        EspWifi::new(modem, sysloop.clone(), Some(nvs)).expect("Failed to initialize WiFi");
-    let wifi_config = ClientConfiguration {
-        ssid: ssid_as_heap_string,
-        password: password_as_heap_string,
-        auth_method: AuthMethod::WPA2Personal,
-        channel: Some(40),
-        scan_method: ScanMethod::FastScan,
-        ..Default::default()
-    };
-    wifi.set_configuration(&Configuration::Client(wifi_config))
-        .expect("Failed to set WiFi");
+    let mut wifi = EspWifi::new(modem, sysloop.clone(), Some(nvs)).expect("Failed to initialize WiFi");
+    let wifi_config = ClientConfiguration {ssid: ssid_as_heap_string, password: password_as_heap_string, auth_method: AuthMethod::WPA2Personal, channel: Some(40), scan_method: ScanMethod::FastScan, ..Default::default()};
+    wifi.set_configuration(&Configuration::Client(wifi_config)).expect("Failed to set WiFi");
 
     wifi.start()?;
     anyhow::Ok(wifi)
 }
 
-fn wifi_connection_event(
-    mut wifi: EspWifi<'static>,
-    wifi_connected: Arc<AtomicBool>,
-) -> anyhow::Result<()> {
+fn wifi_connection_event(mut wifi: EspWifi<'static>, wifi_connected: Arc<AtomicBool>,) -> anyhow::Result<()> {
     loop {
         let is_ready = wifi.is_connected()? && wifi.is_up()?;
         // Check current status
@@ -70,12 +57,7 @@ fn wifi_connection_event(
                 wifi_connected.store(true, Ordering::Relaxed);
             }
             false => {
-                // Disconnected, try to reconnect
-                if wifi_connected.load(Ordering::Relaxed) {
-                    info!("WiFi disconnected! Reconnecting...");
-                    wifi_connected.store(false, Ordering::Relaxed);
-                }
-
+                wifi_connected.store(false, Ordering::Relaxed);
                 match wifi.connect() {
                     Ok(_) => {
                         info!("WiFi reconnection initiated");
@@ -89,25 +71,14 @@ fn wifi_connection_event(
     }
 }
 
-fn setup_mqtt() -> anyhow::Result<(
-    EspMqttClient<'static>,
-    esp_idf_svc::mqtt::client::EspMqttConnection,
-)> {
-    let mqtt_config = MqttClientConfiguration {
-        client_id: Some("esp-water-flow"),
-        username: Some(MQTT_USERNAME),
-        password: Some(MQTT_PASSWORD),
-        ..Default::default()
-    };
+fn setup_mqtt() -> anyhow::Result<(EspMqttClient<'static>, esp_idf_svc::mqtt::client::EspMqttConnection)> {
+    let mqtt_config = MqttClientConfiguration {client_id: Some("esp-water-flow"), username: Some(MQTT_USERNAME), password: Some(MQTT_PASSWORD), ..Default::default()};
     let (mqtt_client, mqtt_event_loop) = EspMqttClient::new(MQTT_URL, &mqtt_config)?;
 
     anyhow::Ok((mqtt_client, mqtt_event_loop))
 }
 
-fn mqtt_connection_event(
-    mut mqtt_connection: EspMqttConnection,
-    mqtt_connected: Arc<AtomicBool>,
-) -> anyhow::Result<()> {
+fn mqtt_connection_event(mut mqtt_connection: EspMqttConnection, mqtt_connected: Arc<AtomicBool>) -> anyhow::Result<()> {
     loop {
         if let Ok(event) = mqtt_connection.next() {
             match event.payload() {
@@ -143,30 +114,21 @@ fn main() -> anyhow::Result<()> {
 
     let mut flow_pin = PinDriver::input(peripherals.pins.gpio25)?;
     flow_pin.set_pull(Pull::Up)?;
-    flow_pin.set_interrupt_type(InterruptType::PosEdge)?;
-    unsafe {
-        flow_pin.subscribe(|| {
-            PULSE_COUNT.fetch_add(1, Ordering::Relaxed);
-        })?;
-    }
-    flow_pin.enable_interrupt()?;
+    flow_pin.set_interrupt_type(InterruptType::AnyEdge)?;
+    unsafe {flow_pin.subscribe(|| {PULSE_COUNT.fetch_add(1, Ordering::Relaxed);})?;}
     info!("Flow sensor reading started on GPIO 25 - counting pulses immediately");
 
     // Initialize WiFi and MQTT
     let wifi = setup_wifi(peripherals.modem)?;
     let (mut mqtt_client, mqtt_event) = setup_mqtt()?;
 
-    let _wifi_thread = std::thread::Builder::new()
-        .stack_size(8192)
-        .spawn(move || {
+    let _wifi_thread = std::thread::Builder::new().stack_size(8192).spawn(move || {
             if let Err(e) = wifi_connection_event(wifi, wifi_connected_clone) {
                 info!("WiFi connection thread error: {:?}", e);
             }
         })?;
 
-    let _mqtt_thread = std::thread::Builder::new()
-        .stack_size(8192)
-        .spawn(move || {
+    let _mqtt_thread = std::thread::Builder::new().stack_size(8192).spawn(move || {
             if let Err(e) = mqtt_connection_event(mqtt_event, mqtt_connected_clone) {
                 info!("MQTT connection thread error: {:?}", e);
             }
@@ -178,33 +140,27 @@ fn main() -> anyhow::Result<()> {
     let mut last_sample_time = time_now_in_millis();
     let last_pulse_count: u32 = PULSE_COUNT.load(Ordering::Relaxed);
     loop {
-        if time_now_in_millis() - last_sample_time < 1_000 {
-            continue;
-        }
+        flow_pin.enable_interrupt()?;
+        if time_now_in_millis() - last_sample_time < 1_000 {continue;}
 
-        flow_pin.disable_interrupt()?;
         let now = time_now_in_millis();
         let pulses = PULSE_COUNT.load(Ordering::Relaxed);
-        flow_pin.enable_interrupt()?;
 
-        if wifi_connected.load(Ordering::Relaxed) && mqtt_connected.load(Ordering::Relaxed) {
-            let time_delta = now - last_sample_time;
-            let pulse_delta = pulses.saturating_sub(last_pulse_count);
-            let payload = json!({"total_pulses": pulse_delta, "Time_ms": time_delta});
-            match mqtt_client.publish(
-                MQTT_TOPIC,
-                QoS::AtLeastOnce,
-                false,
-                payload.to_string().as_bytes(),
-            ) {
+        if !wifi_connected.load(Ordering::Relaxed) || !mqtt_connected.load(Ordering::Relaxed) {continue;}
+
+        let time_delta = now - last_sample_time;
+        let pulse_delta = pulses.saturating_sub(last_pulse_count);
+        let payload = json!({"total_pulses": pulse_delta, "Time_ms": time_delta});
+        
+        match mqtt_client.publish(MQTT_TOPIC,QoS::AtLeastOnce,false, payload.to_string().as_bytes()) {
                 Ok(_) => {
-                    info!("Published total pulses: {}", pulses);
+
                     last_sample_time = now;
                 }
                 Err(e) => {
                     info!("Failed to publish data: {:?}", e);
-                }
             }
         }
     }
 }
+
